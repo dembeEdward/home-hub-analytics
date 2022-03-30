89371da411a8a4383bfd0b8e0de57308eab8673e
Dembe Tshifaro <dembe.tshifaro@universal.one>
2022-03-30 19:56:27 +0200

    updated user permission joins

--- schema/Users-permissionsPermission.js
+++ schema/Users-permissionsPermission.js
@@ -1,54 +1,61 @@
 cube(`Users_permissionsPermission`, {
   sql: `SELECT * FROM public."users-permissions_permission"`,
-  
+
   preAggregations: {
     // Pre-Aggregations definitions go here
-    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
   },
-  
+
   joins: {
-    
+    Profiles: {
+      sql: `${CUBE}.profile = ${Profiles}.id`,
+      relationship: `hasOne`,
+    },
+    Users_permissionsRole: {
+      sql: `${CUBE}.role = ${Users_permissionsRole}.id`,
+      relationship: `hasOne`,
+    },
   },
-  
+
   measures: {
     count: {
       type: `count`,
-      drillMembers: [id]
-    }
+      drillMembers: [id],
+    },
   },
-  
+
   dimensions: {
     id: {
       sql: `id`,
       type: `number`,
-      primaryKey: true
+      primaryKey: true,
     },
-    
+
     enabled: {
       sql: `enabled`,
-      type: `string`
+      type: `string`,
     },
-    
+
     type: {
       sql: `type`,
-      type: `string`
+      type: `string`,
     },
-    
+
     controller: {
       sql: `controller`,
-      type: `string`
+      type: `string`,
     },
-    
+
     action: {
       sql: `action`,
-      type: `string`
+      type: `string`,
     },
-    
+
     policy: {
       sql: `policy`,
-      type: `string`
-    }
+      type: `string`,
+    },
   },
-  
-  dataSource: `default`
+
+  dataSource: `default`,
 });
